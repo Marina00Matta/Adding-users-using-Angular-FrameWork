d70611615d67731d254c1d3ac9543cef9e99da09
marina00matta <marinamedhat_19@hotmail.com>
2020-04-03 22:36:26 +0200

    applying databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-demo';
+  title = 'ITI Course';
+  username = 'marina';
+  NameAddMessage = "no user is added"
+  constructor() {}
+  
+  onInput(ev){
+    console.log(ev.target.value);
+    this.username = ev.target.value;
+  }
+
+  onClick(ev){
+    console.log(ev);
+    this.NameAddMessage = `${this.username} is Added`;
+
+  }
+
+  
 }
